2a78fc188fcefebb5241246025152157b2ee6b55
Irene <irene.engg2011@gmail.com>
2025-03-02 18:44:00 -0500

    Updated Fargate

---
+++ modules/fargate/main.tf
@@ -0,0 +1,83 @@
+resource "aws_ecs_cluster" "cloud_cluster" {
+  name = "cloud-cluster"
+}
+
+resource "aws_ecs_task_definition" "frontend_task" {
+  family                   = "frontend-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 512
+  memory                   = 1024
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "frontend-container"
+      image     = "nginx:latest"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "backend_task" {
+  family                   = "backend-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 512
+  memory                   = 1024
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "backend-container"
+      image     = "amazonlinux"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 5000
+          hostPort      = 5000
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "frontend_service" {
+  name            = "frontend-service"
+  cluster         = aws_ecs_cluster.cloud_cluster.id
+  task_definition = aws_ecs_task_definition.frontend_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 2
+
+  network_configuration {
+    subnets         = var.private_subnets
+    security_groups = [aws_security_group.frontend_sg.id]
+    assign_public_ip = false
+  }
+}
+
+resource "aws_ecs_service" "backend_service" {
+  name            = "backend-service"
+  cluster         = aws_ecs_cluster.cloud_cluster.id
+  task_definition = aws_ecs_task_definition.backend_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 2
+
+  network_configuration {
+    subnets         = var.private_subnets
+    security_groups = [aws_security_group.backend_sg.id]
+    assign_public_ip = false
+  }
+}
