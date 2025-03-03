a07b3b42cd64bc3c275121299b48bf602c929361
irene-engg <irene.engg2011@gmail.com>
2025-03-03 12:04:19 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region for deployment"
   type        = string
-  default     = "us-east-1"
+  default     = "ca-central-1"
 }
 
 variable "vpc_cidr_block" {
