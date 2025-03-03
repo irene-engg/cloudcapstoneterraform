e5c0452af1fd204de56f90ea952392fee3d54ec5
irene-engg <irene.engg2011@gmail.com>
2025-03-03 12:30:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,12 +14,12 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./.modules/vpc"
+  source = "./modules/vpc"
   vpc_cidr_block = var.vpc_cidr_block
 }
 
 module "fargate" {
-  source          = "./.modules/fargate"
+  source          = "./modules/fargate"
   vpc_id         = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnet_ids
 }
