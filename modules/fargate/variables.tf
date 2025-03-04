2549d9153590f6d9a3040801502cab628cc9e3aa
irene-engg <irene.engg2011@gmail.com>
2025-03-04 13:49:17 -0500

    Update variables.tf

--- modules/fargate/variables.tf
+++ modules/fargate/variables.tf
@@ -3,7 +3,37 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "private_subnets" {
-  description = "List of private subnets"
-  type        = list(string)
+#variable "private_subnets" {
+  #description = "List of private subnets"
+  #type        = list(string)
+#}
+
+variable "frontend_subnet1" {
+  description = "Front end private subnet1"
+  type        = string
+}
+
+variable "frontend_subnet2" {
+  description = "Front end private subnet2"
+  type        = string
+}
+
+variable "backend_subnet1" {
+  description = "Back end private subnet1"
+  type        = string
+}
+
+variable "backend_subnet2" {
+  description = "Back end private subnet2"
+  type        = string
+}
+
+variable "data_subnet1" {
+  description = "Data private subnet1"
+  type        = string
+}
+
+variable "data_subnet2" {
+  description = "Data private subnet2"
+  type        = string
 }
