82ef792495ed3c0a140f5c6836c6978e3c4ca36e
irene-engg <irene.engg2011@gmail.com>
2025-03-04 13:25:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,18 +9,20 @@ terraform {
   required_version = ">= 1.6.6"
 }
 
+# To create the resources in the specified region
 provider "aws" {
   region = var.aws_region
 }
 
+# To run the VPC module
 module "vpc" {
   source = "./modules/vpc"
   vpc_cidr_block = var.vpc_cidr_block
 }
 
-module "fargate" {
-  source          = "./modules/fargate"
-  vpc_id         = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnet_ids
-}
+#module "fargate" {
+  #source          = "./modules/fargate"
+  #vpc_id         = module.vpc.vpc_id
+  #private_subnets = module.vpc.private_subnet_ids
+#}
 
