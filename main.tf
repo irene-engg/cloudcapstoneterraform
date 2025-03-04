ae42bab25d31a210d2ec1ed0767fb94248ab811e
irene-engg <irene.engg2011@gmail.com>
2025-03-04 10:54:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,4 +18,9 @@ module "vpc" {
   vpc_cidr_block = var.vpc_cidr_block
 }
 
+module "fargate" {
+  source          = "./modules/fargate"
+  vpc_id         = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnet_ids
+}
 
