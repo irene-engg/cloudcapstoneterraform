25e5636df3e343e0d2fd9deb2ca8f4bd525fb9c8
irene-engg <irene.engg2011@gmail.com>
2025-03-06 11:48:54 -0500

    Update input.tf

--- modules/vpc/input.tf
+++ modules/vpc/input.tf
@@ -7,7 +7,6 @@ variable "aws_region" {
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
