7ab759dae79b83876a846b0d65bd34a2ba8a4da2
irene-engg <irene.engg2011@gmail.com>
2025-03-06 15:43:27 -0500

    Update input.tf

--- input.tf
+++ input.tf
@@ -1,11 +1,9 @@
 variable "aws_region" {
   description = "AWS region selected for deployment- Canada Central"
   type        = string
-  default     = "ca-central-1"
 }
 
 variable "vpc_cidr_block" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
