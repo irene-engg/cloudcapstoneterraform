35935e18a2e4402796d75d44eb6f2d175504ade5
irene-engg <irene.engg2011@gmail.com>
2025-03-06 15:29:13 -0500

    Update iam.tf

--- modules/fargate/iam.tf
+++ modules/fargate/iam.tf
@@ -1,5 +1,10 @@
-resource "aws_iam_role" "ecs_execution_role1" {
+data "aws_iam_role" "existing_ecs_execution_role1" {
   name = "ecs-execution-role1"
+}
+
+resource "aws_iam_role" "ecs_execution_role1" {
+  count = length(data.aws_iam_role.existing_ecs_execution_role1.arn) > 0 ? 0 : 1
+  name  = "ecs-execution-role1"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -16,6 +21,7 @@ resource "aws_iam_role" "ecs_execution_role1" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_execution_role1_policy" {
-  role       = aws_iam_role.ecs_execution_role1.name
+  count      = length(data.aws_iam_role.existing_ecs_execution_role1.arn) > 0 ? 0 : 1
+  role       = aws_iam_role.ecs_execution_role1[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
