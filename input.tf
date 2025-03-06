5333cec111b36958c720532e6ca5ed22570d35f0
irene-engg <irene.engg2011@gmail.com>
2025-03-06 18:35:44 -0500

    Update input.tf

--- input.tf
+++ input.tf
@@ -2,8 +2,3 @@ variable "aws_region" {
   description = "AWS region selected for deployment- Canada Central"
   type        = string
 }
-
-variable "vpc_cidr_block" {
-  description = "CIDR block for VPC"
-  type        = string
-}
